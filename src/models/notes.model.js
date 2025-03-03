4c6492b40b026ac3751dbb7384eaa1467f52116c
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-03-03 22:33:43 +0530

    Add db model files

---
+++ src/models/notes.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const notesSchema = new Schema({
+    User: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        required: true,
+        default: "General"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+export const Note = mongoose.model("Note", notesSchema)
---
+++ src/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+export const User = mongoose.model("User", userSchema)
