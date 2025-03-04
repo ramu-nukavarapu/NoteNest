779c90d0b7015b5b2b91b6775a818089fcb5a7bd
ramu-nukavarapu <ramunukavarapu2004@gmail.com>
2025-03-04 22:35:16 +0530

    Update index.js file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./configs/db.config.js";
 import { userRouter } from "./routes/user.route.js";
+import { notesRouter } from "./routes/notes.route.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 connectDB();
 
-app.use(userRouter)
+app.use(userRouter);
+app.use(notesRouter);
 
 app.listen(PORT, () => console.log("Listening from the port: " + PORT));
